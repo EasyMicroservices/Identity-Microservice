2a5d6cee12d4fa019dbfa7c62fd9268fe29086ee
Mahdiyar Ghannad <m.gh.1385.mahdiyar@gmail.com>
2023-11-27 16:32:21 +0330

    Add AppInit request

---
+++ src/CSharp/EasyMicroservices.IdentityMicroservice.Domain/Contracts/Responses/ApplicationInitializeResponseContract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.IdentityMicroservice.Contracts.Responses;
+
+{
+    public class ApplicationInitializeResponseContract
+    {
+        public bool IsLogin { get; set; }
+        //public bool IsAdmin { get; set; }
+    }
+}
