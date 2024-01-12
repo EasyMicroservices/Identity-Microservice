4234eb929797341fdbceb977b3e82e8828b6887e
Ali Yousefi <ali.visual.studio@gmail.com>
2024-01-12 17:59:59 +0330

    Add support test packages for identity

---
+++ src/CSharp/EasyMicroservices.IdentityMicroservice.Tests/Fixtures/AuthenticationTestFixture.cs
@@ -0,0 +1,37 @@
+﻿using EasyMicroservices.Cores.AspEntityFrameworkCoreApi;
+using EasyMicroservices.IdentityMicroservice.BackgroundServices;
+using EasyMicroservices.IdentityMicroservice.Database.Contexts;
+using EasyMicroservices.IdentityMicroservice.Interfaces;
+using EasyMicroservices.IdentityMicroservice.WebApi;
+using EasyMicroservices.IdentityMicroservice.WebApi.Controllers;
+using Identity.GeneratedServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyMicroservices.IdentityMicroservice.Tests.Fixtures;
+public class AuthenticationTestFixture : IAsyncLifetime
+{
+    public IServiceProvider ServiceProvider { get; private set; }
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task InitializeAsync()
+    {
+        var app = Program.CreateBuilder(null);
+        string baseUrl = $"http://localhost:{2007}";
+        app.Services.AddSingleton(s => new HttpClient());
+        app.Services.AddTransient(s => new AuthenticationClient(baseUrl, s.GetService<HttpClient>()));
+        app.Services.AddMvc().AddApplicationPart(typeof(AuthenticationController).Assembly);
+
+        var build = await app.Build<IdentityContext>(true);
+        build.MapControllers();
+        using var scope = build.Services.CreateScope();
+        var unitOfWork = scope.ServiceProvider.GetService<IAppUnitOfWork>();
+        await InternalTokenGeneratorBackgroundService.GetToken(unitOfWork);
+        ServiceProvider = app.Services.BuildServiceProvider();
+        _ = build.RunAsync();
+    }
+}
